c7893e73a7730fc1dc341d0c39488e0cc0b7d2a7
Wes Modes <wmodes@gmail.com>
2020-11-16 15:10:48 -0800

    Added lab 20

--- lab20/js/index.js
+++ lab20/js/index.js
@@ -1,17 +1,21 @@
+// Comment block here
+// Author:
+// Date:
 
-// URL = "https://www.boredapi.com/api/activity/"
-// URL = "https://api.kanye.rest/"
-URL = "https://api.whatdoestrumpthink.com/api/v1/quotes/random"
+
+URL = "https://api.nasa.gov/planetary/apod"
 
 // attach click action to button
-$('#action').click(function(){
+// $('#action').click(function(){
     // get data via ajax from numbersapi
     // Using the core $.ajax() method
     $.ajax({
         // The URL for the request (ENDPOINT)
         url: URL,
         // The data to send (will be converted to a query string)
-        // data: { id: 123},
+        data: {
+          api_key: "HfxW4diusQBshk1BFUjCC60o8SFnxjcbdLEsKHRP"
+        },
         // Whether this is a POST or GET request
         type: "GET",
         // The type of data we expect back
@@ -24,7 +28,12 @@ $('#action').click(function(){
         // $("#output").append("<p>" + JSON.stringify(data));
         // $("#output").append("<p>Here's what you should do when you are bored: <b>" + data.activity);
         // $("#output").append("<p>President-elect Kanye says: <b>" + data.quote);
-        $("#output").append("<p>The most stable smart man in the room says: <b>" + data.message);
+        // $("#output").append("<p>" + JSON.stringify(data));
+        imageUrl = data.url;
+        console.log("URL:", imageUrl);
+        $("#output").append("<div id='apod'><img src='" + imageUrl + "'>");
+        $("#output").append("<div id='date'>" + data.date);
+        $("#output").append("<div id='caption'>" + data.explanation);
 
     })
-});
+// });
