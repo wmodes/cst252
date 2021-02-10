e0dbcc2bdd4a139d64e9dfbef08f8954ec782a0f
Wes Modes <wmodes@gmail.com>
2021-02-10 11:43:28 -0800

    lab5 revision

---
+++ experiments/tic-tac-toe/js/lab.js
@@ -0,0 +1,16 @@
+// tic-tac-toe.js
+// A tic-tac-toe game against the computer
+//
+// Author: Wes Modes <wmodes@csumb.edu>
+// Date: Feb 2021
+// License: MIT 2.0
+
+
+// Print the board before each turn
+    // Loop through each row
+        // Loop through each space
+            // Print X, O, or Space
+            // Print a | between
+        // Print a newline at the end
+        // Print a horizontal line between
+//
--- lab5/js/lab5.js
+++ lab5/js/lab5.js
@@ -1,23 +1,31 @@
-/*
- * Author: Wes Modes <wmodes@ucsc.edu>
- * Created: 13 September
- * License: Public Domain
- */
+// lab5.js - Play with variables and stuff
+// Author: Wes Modes <wmodes@ucsc.edu>
+// Created: 13 September
+// License: Public Domain
+
 
 console.log("We got this far, yo")
 
-// Define Variables
-make = "Ford";
-model = "Taurus";
-color = "Rusty";
-year = 1995;
 
-// calculate
-age = 2019 - year;
+// Declare variables to hold the make, model, color, and year of your vehicle, example, var year = 2004
+var make = "Ford";
+var model = "Taurus";
+var color = "rusty";
+var year = 1995;
+var extras = "torn hardtop, ripped upholstery";
 
-// output
+
+// Declare a boolean variable ownIt for whether you own it or not.
+var ownIt = true;
+
+// Create a new variable age that subtracts the year from the current year
+var age = new Date().getFullYear();
+
+// Use document.writeln() to neatly output each of your variables, example, document.writeln("Make: " + make + "<br>");
 document.writeln("Make: " + make + "<br>");
 document.writeln("Model: " + model + "<br>");
-document.writeln("Color: " + color + "<br>");
+document.writeln("Extras: " + extras + "<br>");
 document.writeln("Year: " + year + "<br>");
-document.writeln("Age: " + age + " years<br>");
+document.writeln("Age: " + age + "<br>");
+
+document.writeln("");
