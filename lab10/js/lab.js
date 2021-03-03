7de2b5d93d0919dbb77766cb5eab0d23370d6e64
Wes Modes <wmodes@gmail.com>
2021-03-03 11:57:52 -0800

    Lab 10

---
+++ lab10/js/lab.js
@@ -0,0 +1,61 @@
+// Lab 10 - sort or scramble a name
+// Author: S Llawom Eydansele <wmodes@csumb.edu>
+// Created: 19 September
+// License: Public Domain
+
+// shuffleArray - take an array and shuffle the contents
+// Thanks to https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffleArray(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+// sortUserName - a function that takes user input and sorts the letters
+// of their name
+function reorderUserName(word) {
+    var wordArray = word.toLowerCase().split('');
+    var newArray = shuffleArray(wordArray);
+    return newArray.join('');
+}
+
+// given a string, return string in Title Case
+// thanks to https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
+
+// find the button element
+buttonEl = document.getElementById("my-button");
+console.log("button element:", buttonEl);
+// find the form element
+inputEl = document.getElementById("user-name");
+console.log("input element:", inputEl);
+// find output element
+outputEl = document.getElementById("output");
+console.log("output element:", outputEl);
+
+// add an event listener to button
+buttonEl.addEventListener("click", function(){
+  // get value from name element
+  var userName = inputEl.value;
+  // modify value - either sort or shuffle
+  var newName = toTitleCase(reorderUserName(userName));
+  // put value in output element
+  outputEl.innerHTML = "<p id=name-results>" + newName + "</p>";
+});
